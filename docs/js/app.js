8b6231ac21b03224ecba060452ef066b7858d3f5
wangbin <316281400@qq.com>
2021-08-17 11:27:01 +0800

    发布新版文档

--- docs/js/app.js
+++ docs/js/app.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "js/" + ({"0":"DOCS0","1":"DOCS1","2":"DOCS2"}[chunkId]||chunkId) + "." + "7c63122" + ".js"
+/******/ 		return __webpack_require__.p + "js/" + ({"0":"DOCS0","1":"DOCS1","2":"DOCS2"}[chunkId]||chunkId) + "." + "6c42a2d" + ".js"
 /******/ 	}
 /******/
 /******/ 	// The require function
